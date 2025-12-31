babfa7bb95e7805910bbc9b4b9f1f8a853587d4a
Vanshi1107 <vanshika.maheshwari1107@gmail.com>
2025-12-31 13:47:50 +0530

    Added comments in ocr_part.py for clarity

--- Prototype/backend/ocr_part.py
+++ Prototype/backend/ocr_part.py
@@ -2,14 +2,14 @@ from PIL import Image
 import pytesseract
 import io
 
-pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe" 
 
 
 
 def extract_text_from_image(image_bytes: bytes) -> str:
-    """
-    Takes image bytes and returns extracted text using OCR.
-    """
+    
+    #Takes image bytes and returns extracted text using OCR.
+    
     image = Image.open(io.BytesIO(image_bytes))
-    text = pytesseract.image_to_string(image)
+    text = pytesseract.image_to_string(image) #Extract text info from image
     return text
