23baccce3c29b6545a4f00b0833b5df07bb7d697
Vanshi1107 <vanshika.maheshwari1107@gmail.com>
2026-01-11 22:53:33 +0530

    Added comments
    Added comments for clarity and consistency in main.py.

--- Backend/main.py
+++ Backend/main.py
@@ -5,27 +5,27 @@ from OCR_part import extract_text_from_image
 from utils.amount_fallback import extract_amount_fallback
 
 app = FastAPI(title="Expense Extraction API")
-print("=== MAIN.PY LOADED ===")
 
 
 
-# ---------- CORS (needed for web frontend) ----------
+
+# CORS (needed for web frontend)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],   # tighten later if needed
+    allow_origins=["*"],   
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-# ---------- Health Check ----------
+# Checks if the backend is working
 
 @app.get("/")
 def health_check():
     return {"status": "Backend is running"}
 
-# ---------- Text-based Expense Parsing ----------
+#Text-based Expense Parsing
 
 @app.post("/parse-expense")
 def parse_expense(
@@ -37,7 +37,7 @@ def parse_expense(
     """
     return extract_expense(text=text, source=source)
 
-# ---------- OCR-based Expense Parsing ----------
+# OCR-based Expense Parsing 
 
 @app.post("/parse-expense-image")
 async def parse_expense_image(file: UploadFile = File(...)):
